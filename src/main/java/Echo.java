945841eb56997d04270e91ddd39914e17ea02a86
kah-seng <71002797+kah-seng@users.noreply.github.com>
2026-01-19 15:05:19 +0800

    Add user command echo

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Echo {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n"
-                + "Hello! I'm Echo.\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n");
+        System.out.println("""
+                ------------------------------------------------------------
+                Hello! I'm Echo.
+                What can I do for you?
+                ------------------------------------------------------------
+                """);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println("\n------------------------------------------------------------\n"
+                    + userInput + "\n"
+                    + "------------------------------------------------------------\n");
+        }
+
+        System.out.println("""
+                \n------------------------------------------------------------
+                Bye. Hope to see you again soon!
+                ------------------------------------------------------------
+                """);
     }
 }
