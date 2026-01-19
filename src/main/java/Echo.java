ce5592b943c7fb215cbcdbd4b66daf75739c51e4
kah-seng <71002797+kah-seng@users.noreply.github.com>
2026-01-19 15:37:05 +0800

    Add list command

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Echo {
@@ -5,13 +6,19 @@ public class Echo {
         System.out.print(Message.getWelcomeMessage());
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> userInputs = new ArrayList<>();
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 break;
             } else if (userInput.equals("list")) {
-
+                System.out.println("\n" + Message.DIVIDER);
+                for (int i = 0; i < userInputs.size(); i++) {
+                    System.out.printf("%d. %s\n", i + 1, userInputs.get(i));
+                }
+                System.out.println(Message.DIVIDER + "\n");
             } else {
+                userInputs.add(userInput);
                 System.out.print(Message.getAddedMessage(userInput));
             }
         }
