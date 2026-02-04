0785f32427d8e3b246184bfd4ccba0ac54e820f2
kah-seng <71002797+kah-seng@users.noreply.github.com>
2026-02-04 16:44:33 +0800

    Refactor to FXML

---
+++ src/main/java/echo/MainWindow.java
@@ -0,0 +1,45 @@
+package echo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Echo echo;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image echoImage = new Image(this.getClass().getResourceAsStream("/images/Echo.png"));
+
+    @FXML
+    private void initialize() {
+        this.scrollPane.vvalueProperty().bind(this.dialogContainer.heightProperty());
+    }
+
+    public void setEcho(Echo echo) {
+        this.echo = echo;
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String userInput = this.userInput.getText();
+        String response = this.echo.getResponse(userInput);
+        this.dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userInput, this.userImage),
+                DialogBox.getEchoDialog(response, this.echoImage)
+        );
+        this.userInput.clear();
+    }
+}
